384e170b71ad7405e8cf9ac6887633fbcbe83fe1
lindsay-greene <lindsay.smile@yahoo.com>
2020-09-24 20:22:53 -0400

    corrected code from template repo

--- js/components/list.js
+++ js/components/list.js
@@ -2,36 +2,46 @@ import Component from '../lib/component.js';
 import store from '../store/index.js';
 
 export default class List extends Component {
-
-  constructor() {
-    super({
-      store,
-      element: document.querySelector('.js-items')
-    });
-  }
-
-  render() {
-    let self = this;
-    // Message to user if list has no items
-    if(store.state.items.length === 0) {
-      self.element.innerHTML = `<p class="no-items">You've done nothing yet &#x1f622;</p>`;
-      return;
+    
+    // Pass our store instance and the HTML element up to the parent Component
+    constructor() {
+        super({
+            store,
+            element: document.querySelector('.js-items')
+        });
     }
-    // Print list of items when there are items 
-    self.element.innerHTML = `
-      <ul class="app__items">
-        ${store.state.items.map(item => {
-          return `
-            <li>${item}<button aria-label="Delete this item">×</button></li>
-          `
-        }).join('')}
-      </ul>
-    `;
-    // Button attached to each item
-    self.element.querySelectorAll('button').forEach((button, index) => {
-      button.addEventListener('click', () => {
-        store.dispatch('clearItem', { index });
-      });
-    });
-  }
+
+    /**
+     * React to state changes and render the component's HTML
+     *
+     * @returns {void}
+     */
+    render() {
+        let self = this;
+
+        // If there are no items to show, render a little status instead
+        if(store.state.items.length === 0) {
+            self.element.innerHTML = `<p class="no-items">You've done nothing yet 😢</p>`;
+            return;
+        }
+        
+        // Loop the items and generate a list of elements
+        self.element.innerHTML = `
+            <ul class="app__items">
+                ${store.state.items.map(item => {
+                    return `
+                        <li>${item}<button aria-label="Delete this item">×</button></li>
+                    `
+                }).join('')}
+            </ul>
+        `;
+        
+        // Find all the buttons in the list and when they are clicked, we dispatch a 
+        // `clearItem` action which we pass the current item's index to
+        self.element.querySelectorAll('button').forEach((button, index) => {
+            button.addEventListener('click', () => {
+                store.dispatch('clearItem', { index });
+            });
+        });
+    }
 };
--- js/main.js
+++ js/main.js
@@ -1,30 +1,35 @@
-// Importing what we need from other documents 
 import store from './store/index.js'; 
+
+// Load up components
 import Count from './components/count.js';
 import List from './components/list.js';
 import Status from './components/status.js';
+
+// Load up some DOM elements
 const formElement = document.querySelector('.js-form');
 const inputElement = document.querySelector('#new-item-field');
 
-// Make the form interactive 
+// Add a submit event listener to the form and prevent it from posting back
 formElement.addEventListener('submit', evt => {
-    // prevent textbox from submitting until we check it 
     evt.preventDefault();
-    // trim textbox entry to make sure there is actual content 
+    
+    // Grab the text value of the textbox and trim any whitespace off it
     let value = inputElement.value.trim();
-    // if entry is not empty then dispatch it to store 
+    
+    // If there's some content, trigger the action and clear the field, ready for the next item
     if(value.length) {
-      store.dispatch('addItem', value);
-      inputElement.value = '';
-      inputElement.focus();
+        store.dispatch('addItem', value);
+        inputElement.value = '';
+        inputElement.focus();
     }
-  });
+});
 
-// create and render new instances of each component 
+// Instantiate components
 const countInstance = new Count();
 const listInstance = new List();
 const statusInstance = new Status();
 
+// Initial renders
 countInstance.render();
 listInstance.render();
 statusInstance.render();
